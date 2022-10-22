589106b74913b53623e5d670835e56d6767ab0be
angris-2 <angris-2>
2022-10-22 16:36:06 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded",
+    function(e){
+        function age() {
+            var round = document.getElementById("round").value;
+            var result = parseInt(round)/6;
+            document.getElementById("round").value = result+"세";
+        }
+
+        let button=document.querySelector("button")
+        button.addEventListener("click",
+            function(e){
+                age()
+            }
+        )
+    
+        button.addEventListener("mouseover",
+            function(e){
+                e.target.style.backgroundColor ="rgb(200, 130, 0)" 
+            }
+        )
+
+        button.addEventListener("mouseleave",
+            function(e){
+                e.target.style.backgroundColor="orange"
+            }
+        )
+    }
+)
+
